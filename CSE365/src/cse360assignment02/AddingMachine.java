c0fd3d636017f513243dd2d5ab13d7f7e335c8c5
franz <franz@DESKTOP-1ECH4B7>
2020-10-03 15:52:04 -0700

    Second Commit

--- CSE365/src/cse360assignment02/AddingMachine.java
+++ CSE365/src/cse360assignment02/AddingMachine.java
@@ -6,49 +6,67 @@ public class AddingMachine
 {
 	/*
 	 total represents the total after the desired calculations
+	 history represents all the history of the adding machine operations
 	 */
 	private int total;
+	private String history = "0";
 	/*
-  constructor, creates an adding machine object and sets the total count to 0
+  constructor, creates an adding machine object and then adds 4, 
 	 */
+	public static void main(String []args)
+	{
+		
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add (4); 
+		myCalculator.subtract (2); 
+		myCalculator.add(5);
+		System.out.println(myCalculator.toString());
+		
+		
+	}
 	public AddingMachine () 
 	{
 		total = 0;  // not needed - included for clarity
 	}
 	/*
-   gets the total, @return 0
+   gets the total, @return total
 	 */
 	public int getTotal () 
 	{
-		return 0;
+		return total;
 	}
 	/*
    	adds a value @param value adds a value to previous total
+   	history adds its previous self to its new calculations
 	 */
 	public void add (int value) 
 	{
-
+		total = total + value; 
+		history += " + " + value;
 	}
 	/*
 	subtracts a value @param value subtracts a value to previous total
+	history adds its previous self to its new calculations
 	 */
 	public void subtract (int value) 
 	{
-
+		total = total - value;
+		history += " - " + value;
 	}
 	/*
 	thus function prints out the history of transactions 
-	@return ""
+	@return the history of the adding machine
 	 */
 	public String toString ()
 	{
-		return "";
+		return history + " ";
 	}
 	/*
-	this method clears the memory
+	this method clears the memory of the adding machine, setting history to empty and total to 0
 	 */
 	public void clear() 
 	{
-
+		total = 0;
+		history = "";
 	}
 }
