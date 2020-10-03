8f68afcfd4f465235908aa6ac48e8fec276a2095
franz <franz@DESKTOP-1ECH4B7>
2020-10-03 14:59:30 -0700

    Initial Commit

---
+++ CSE365/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assignment02;
+/*
+ Represents an adding machine with the ability to add, subtract, and get total.
+ */
+public class AddingMachine
+{
+	/*
+	 total represents the total after the desired calculations
+	 */
+	private int total;
+	/*
+  constructor, creates an adding machine object and sets the total count to 0
+	 */
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	/*
+   gets the total, @return 0
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	/*
+   	adds a value @param value adds a value to previous total
+	 */
+	public void add (int value) 
+	{
+
+	}
+	/*
+	subtracts a value @param value subtracts a value to previous total
+	 */
+	public void subtract (int value) 
+	{
+
+	}
+	/*
+	thus function prints out the history of transactions 
+	@return ""
+	 */
+	public String toString ()
+	{
+		return "";
+	}
+	/*
+	this method clears the memory
+	 */
+	public void clear() 
+	{
+
+	}
+}
---
+++ CSE365/src/module-info.java
@@ -0,0 +1,2 @@
+module CSE365 {
+}
